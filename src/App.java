fa1bf29b0325112d2023c51f54c2c15adb3d433c
Andres Sepulveda <bodyweightforce@gmail.com>
2023-03-22 00:18:55 -0300

    Validacion en descuentos legales

--- src/App.java
+++ src/App.java
@@ -54,44 +54,61 @@ public class App {
     }
     
     public static void main(String[] args) throws Exception {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingrese el numero de horas trabajadas este mes: ");
-        int normales = scanner.nextInt();
-        System.out.println("Ingrese cuantas horas extras realizo este mes:");
-        int extras = scanner.nextInt();
+        boolean isFuncionando = false;
+        boolean isValido = false;
+        int descuentosAFP = 0;
+        int descuentosMedicos = 0;
 
-        System.out.println("Usted trabajo: " + normales + " horas y realizo: " + extras + " horas extras.");
-        System.out.println("El sueldo bruto de este mes es de: " + sueldoBruto(normales, extras));
-        System.out.println("El bono de marzo es de: " + bonoMarzo(sueldoBruto(normales, extras)));
+        while(!isFuncionando) {
 
-        System.out.println("""
-                Seleccione la AFP en la cual usted se encuentre inscrito:
+            System.out.println("Ingrese el numero de horas trabajadas este mes: ");
+            int normales = scanner.nextInt();
+            System.out.println("Ingrese cuantas horas extras realizo este mes:");
+            int extras = scanner.nextInt();
 
-                    1. CUPRUM 7%
-                    2. MODELO 9%
-                    3. CAPITAL 12%
-                    4. PROVIDA 13%
-                """);
-        int afp = scanner.nextInt();
+            System.out.println("Usted trabajo: " + normales + " horas y realizo: " + extras + " horas extras.");
+            System.out.println("El sueldo bruto de este mes es de: " + sueldoBruto(normales, extras));
+            System.out.println("El bono de marzo es de: " + bonoMarzo(sueldoBruto(normales, extras)));
+
+            while(!isValido) {
+                System.out.println("""
+                    Seleccione la AFP en la cual usted se encuentre inscrito:
+
+                        1. CUPRUM 7%
+                        2. MODELO 9%
+                        3. CAPITAL 12%
+                        4. PROVIDA 13%
+                    """);
+                int afp = scanner.nextInt();
                 System.out.println("""
                     Seleccione el sistema de salud en el cual usted se encuentre inscrito:
-    
+        
                         1. FONASA 12%
                         2. CONSALUD 13%
                         3. MASVIDA 14%
                         4. BANMEDICA 15%
                     """);
-        int medico = scanner.nextInt();
-        scanner.close();
+                    int medico = scanner.nextInt();
+                if ( afp >= 1 && afp <= 4 && medico >= 0 && medico <= 4 ) {
+                        isValido = true;
+                        descuentosAFP = descuentosAFP(sueldoBruto(normales, extras), afp);
+                        descuentosMedicos = descuentosMedicos(sueldoBruto(normales, extras), medico);
+                        break;
+                }
+                else {
+                    System.out.println("Escriba la opcion dentro del rango de 1 - 4.");
+                }
+            }
+            scanner.close();
+            System.out.println("Descuentos de afp: " + descuentosAFP);
+            System.out.println("Descuentos de medico: " + descuentosMedicos);
+            int descuentos = descuentosAFP + descuentosMedicos;
+            int sueldoRecibido = (sueldoBruto(normales, extras) + bonoMarzo(sueldoBruto(normales, extras))) - descuentos ;
 
-        int descuentosAFP = descuentosAFP(sueldoBruto(normales, extras), afp);
-        System.out.println("Descuentos de afp: " + descuentosAFP);
-        int descuentosMedicos = descuentosMedicos(sueldoBruto(normales, extras), medico);
-        System.out.println("Descuentos de medico: " + descuentosMedicos);
-        int descuentos = descuentosAFP + descuentosMedicos;
-        int sueldoRecibido = (sueldoBruto(normales, extras) + bonoMarzo(sueldoBruto(normales, extras))) - descuentos ;
-
-    System.out.println("El pago total de sus servicios este mes es de: " + sueldoRecibido);
+            System.out.println("El pago total de sus servicios este mes es de: " + sueldoRecibido);
+            isFuncionando = true;
+        }
+    
     }
 }
